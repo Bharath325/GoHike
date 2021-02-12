be16dd814e1249c343d2cc46e58075a0a0201eef
Bharath Kishore <bkishore770@gmail.com>
2021-02-12 19:59:24 +0530

    Added a comment

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
-
+// Run express
 const app = express();
 
 app.set('view engine', 'pug');
