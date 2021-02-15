df4838f718c6308001390ca9ba553bf55798ec52
Bharath Kishore <bkishore770@gmail.com>
2021-02-15 21:49:05 +0530

    fixed bug

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,7 @@ const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const signupForm = document.querySelector('.form--signup');
 const logOutBtn = document.querySelector('.nav__el--logout');
+
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
 const bookBtn = document.getElementById('book-tour');
