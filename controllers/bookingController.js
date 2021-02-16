02dc281b0f3a2a68a4698d786680b7ba0f365104
Bharath Kishore <bkishore770@gmail.com>
2021-02-16 09:04:57 +0530

    console logs for debugging

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -53,8 +53,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
+  console.log(tour);
   const user = (await User.findOne({ email: session.customer_email })).id;
+  console.log(user);
   const price = session.line_items[0].amount;
+  console.log(price);
   await Booking.create({ tour, user, price });
 };
 
